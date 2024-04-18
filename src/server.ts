b668ca23adb9132ed8f45f14c0d1cbd2f0b64c12
Matt Hayes <matt@mysterycommand.com>
2024-04-17 22:37:47 -0500

    spike: oh, hello

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,24 @@
 const res = (body?: BodyInit | null, init?: ResponseInit) =>
   new Response(body, init);
 
-Deno.serve(() => res("Hello"));
+const url = (url: string | URL, base?: string | URL | undefined) =>
+  new URL(url, base);
+
+const streamFile = async (path: string) =>
+  (await Deno.open(url(path, import.meta.url), { read: true })).readable;
+
+const upgradeWebSocket = (req: Request) => {
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  socket.addEventListener("open", () => {
+    socket.send("Hello");
+  });
+
+  return response;
+};
+
+Deno.serve(async (req) =>
+  (req.headers.get("upgrade") || "").toLowerCase() === "websocket"
+    ? upgradeWebSocket(req)
+    : res(await streamFile("index.html"))
+);
