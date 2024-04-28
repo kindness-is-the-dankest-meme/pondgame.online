f3de9f398aefeeb398934a6552ac1bafd3cd757e
Matt Hayes <matt@mysterycommand.com>
2024-04-27 23:30:09 -0500

    feat: messing around with custom pointers

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,9 @@
 import { message } from "./lib/message.ts";
-new Worker("./worker.js", { type: "module" });
 console.log(`${message} (client)`);
+
+new Worker("./worker.js", { type: "module" });
+
+// TODO: figure out how to bring lib dom types in here
+// globalThis.addEventListener("pointermove", ({ x, y }) => {
+//   c.style.transform = `translate(${x}px,${y}px)`;
+// });
