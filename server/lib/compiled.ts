f7641f6ef7db07a0daf6239b229a2fca8eb261a6
Matt Hayes <matt@mysterycommand.com>
2025-04-05 15:52:35 -0500

    refactor: deno remove npm:esbuild-wasm, use jsr:@deno/emit

--- server/lib/compiled.ts
+++ server/lib/compiled.ts
@@ -1,6 +1,10 @@
-import { transform } from "esbuild-wasm";
+import { bundle } from "jsr:@deno/emit";
 
-export const compiled = (path: URL) =>
-  Deno.readTextFile(path)
-    .then((source) => transform(source, { loader: "ts" }))
-    .then(({ code }) => code.replace(/"(\..*)\.ts"/g, '"$1.js"'));
+export const compiled = (path: URL) => bundle(path).then(({ code }) => code);
+
+// import { transform } from "esbuild-wasm";
+
+// export const compiled = (path: URL) =>
+//   Deno.readTextFile(path)
+//     .then((source) => transform(source, { loader: "ts" }))
+//     .then(({ code }) => code.replace(/"(\..*)\.ts"/g, '"$1.js"'));
