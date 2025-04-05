1169e62a435eeb120d50d6436e8171933c5c860c
Matt Hayes <matt@mysterycommand.com>
2025-04-04 23:40:26 -0500

    refactor: avoid double render

--- islands/Game.tsx
+++ islands/Game.tsx
@@ -5,6 +5,11 @@ import { Pond } from "../components/Pond.tsx";
 const { floor, random } = Math;
 const range = (a: number, b: number) => floor(a + (random() * (b - a)));
 
+globalThis.addEventListener(
+  "contextmenu",
+  (event: Event) => event.preventDefault(),
+);
+
 export const Game = () => {
   const [size, setSize] = useState<number>(30);
   const [[cols, rows], setDims] = useState<[number, number]>([
@@ -22,24 +27,16 @@ export const Game = () => {
 
     globalThis.addEventListener("click", resetDims);
     globalThis.addEventListener("resize", resetDims, { passive: true });
+
     globalThis.dispatchEvent(new Event("resize"));
+    const intervalId = setInterval(() => setSize(range(20, 100)), 10_000);
 
     return () => {
       globalThis.removeEventListener("click", resetDims);
       globalThis.removeEventListener("resize", resetDims);
+      clearInterval(intervalId);
     };
   }, [size]);
-
-  useEffect(() => {
-    const prevent = (event: Event) => event.preventDefault();
-    globalThis.addEventListener("contextmenu", prevent);
-    return () => globalThis.removeEventListener("contextmenu", prevent);
-  }, []);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => setSize(range(20, 100)), 10_000);
-    return () => clearInterval(intervalId);
-  }, []);
 
   return (
     <>
