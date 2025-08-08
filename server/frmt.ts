583d7edc96bc607c74b0687cea24121925d69234
Matt Hayes <matt@mysterycommand.com>
2025-08-08 17:47:02 -0400

    refactor: just take three arguments

--- server/frmt.ts
+++ server/frmt.ts
@@ -1,10 +1,10 @@
 import { format, join, normalize, type ParsedPath } from "jsr:@std/path";
 
-export const frmt = ({
-  dir,
-  name,
-  ext,
-}: Pick<ParsedPath, "dir" | "name" | "ext">) =>
+export const frmt = (
+  dir: ParsedPath["dir"],
+  name: ParsedPath["name"],
+  ext: ParsedPath["ext"]
+) =>
   normalize(
     format({
       root: "/",
