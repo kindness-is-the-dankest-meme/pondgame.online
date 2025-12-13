1fb6f41a68b458d8b01de98a14f192faf67091f7
Matt Hayes <matt@mysterycommand.com>
2025-12-13 00:30:28 -0600

    chore: call the tw plugin, sort imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import { resolve } from "@std/path/resolve";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve("./src"),
