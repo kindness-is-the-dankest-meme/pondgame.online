027b98bffc9ddec32a0d383c72acc3b15a9221e3
Matt Hayes <matt@mysterycommand.com>
2025-12-13 12:56:47 -0600

    chore: unminify to validate output

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
