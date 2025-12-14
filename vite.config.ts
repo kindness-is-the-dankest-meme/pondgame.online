518bed5e7a3050c8b1f678d5149a00b8b9732b60
Matt Hayes <matt@mysterycommand.com>
2025-12-13 23:11:24 -0600

    chore: remove minify config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    minify: false,
-  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
