aca96c229bad4de2f25707e1e0e87e23cdf6d2d8
Matt Hayes <matt@mysterycommand.com>
2025-12-22 22:17:31 -0600

    refactor: keep things simple until we need more shrinking

--- src/client/entry.tsx
+++ src/client/entry.tsx
@@ -1,9 +1,8 @@
+import { App } from "@/components/App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 declare const m: HTMLElementTagNameMap["main"];
-
-const { App } = await import("@/components/App.tsx");
 
 createRoot(m).render(
   <StrictMode>
